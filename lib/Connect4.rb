55ecbbd569a45df4e98ee0e0da1ec20a1c0b3ce5
DesterStorm <daniel.siekawitch@gmail.com>
2019-09-11 12:39:07 -0500

    commit: CLI Gem connect 4 + web scraper for pro tips.

---
+++ lib/Connect4.rb
@@ -0,0 +1,6 @@
+require "Connect4/version"
+
+module Connect4
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/Connect4/version.rb
@@ -0,0 +1,8 @@
+require 'Connect4/lib/Connect4/version'
+require_relative 'game.rb'
+require_relative 'play.rb'
+require_relative 'scraper.rb'
+
+module Connect4
+  VERSION = '0.1.0'
+end
